b3142996030e1e233ca9577c7c438b4dbe2f6abe
SusanLDavenport <susanldavenport@gmail.com>
2018-03-10 23:17:54 -0500

    javascript additions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,34 @@
+var questionArray = [
+'What is the name of the typeface Wes Anderson uses in his movies?', 
+'Who has Anderson written the most screenplays with?', 
+'Anderson includes a reference to what classic show in all of his movies?', 
+'In Life Aquatic, what artist did guitar player Seu Jeorge pay tribute to throughout multiple scenes of the movie?', 
+'What actor was originally considered for the role of M. Gustave in Grand Budapest Hotel', 
+'In what year does Moonrise Kingdom take place?'];
+
+var answerArray = [
+['Arial', 'Futura', 'Georgia', 'Monotype'], 
+['Bill Murray', 'Angelica Houston', 'Owen Wilson', 'John Malkovich'],
+['The Addams Family', 'Dick Van Dyke', 'Buck Rogers', 'The Peanuts'],
+['Elvis', 'David Bowie', 'Elton John', 'John Lennon'],
+['Kevin Spacey', 'Johnny Depp', 'Stanley Tucci', 'Edward Norton'], 
+['1968', '1967', '1966', '1965']
+];
+
+var correctAnswers = [
+'B. Futura', 
+'C. Owen Wilson', 
+'D. The Peanuts', 
+'B. David Bowie', 
+'B. Johnny Depp', 
+'D. 1965'];
+
+var timer = 5;
+var userGuess;
+var userCorrect = 0;
+
+//______________VARIABLES_____________________
+
 $(document).ready(function() {
 // question timer (20 seconds?)
     function twentySeconds() {
@@ -18,9 +49,25 @@ $(document).ready(function() {
                 clearInterval(intervalId);
             } //end stop function
 
+    //run timer
     twentySeconds();
 
 // display question and answer arrays
+    for( var i = 0; i < questionArray.length; i++) {
+        var question = questionArray[i];
+        console.log(question);
+    }
+    $('#question').append(question);
+
+    for (var i = 0; i < answerArray.length; i++){
+        var answer = answerArray[i];
+        // console.log(answer[i]);
+    }
+
+    $('#answer-a').append(answer[0]);
+    $('#answer-b').append(answer[1]);
+    $('#answer-c').append(answer[2]);
+    $('#answer-d').append(answer[3]);
 
 // create buttons containing answers
 // when user clicks on button ans. change color, move to next question, reset timer
@@ -31,11 +78,5 @@ $(document).ready(function() {
 // after last question, display no. of answers right and wrong
 // link to someone elses game 
 
-var timer = 5;
-var questionArray = ['What is the name of the typeface Wes Anderson uses in his movies?', 'Who has Anderson written the most screenplays with?', 'Anderson includes a reference to what classic show in all of his movies?', 'In Life Aquatic, what artist did guitar player Seu Jeorge pay tribute to throughout multiple scenes of the movie?', 'What actor was originally considered for the role of M. Gustave in Grand Budapest Hotel', 'In what year does Moonrise Kingdom take place?'];
-var answerArray = ['Arial, Futura, Georgia, Monotype', 'Bill Murray, Angelica Houston, Owen Wilson, John Malkovich', 'The Addams Family, Dick Van Dyke, Buck Rogers, The Peanuts', 'Elvis, David Bowie, Elton John, John Lennon', 'Kevin Spacey, Johnny Depp, Stanley Tucci, Edward Norton', '1968, 1967, 1966, 1965'];
-var correctAnswers = ['B. Futura', 'C. Owen Wilson', 'D. The Peanuts', 'B. David Bowie', 'B. Johnny Depp', 'D. 1965'];
-var userGuess;
-var userCorrect = 0;
 // var clickSound = new Audio (meow.mp3); 
 });//end document.ready
